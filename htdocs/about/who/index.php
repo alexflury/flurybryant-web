ffca111ed2e59f277b221a5805da8397edbbf293
Alex Flury <aflury@bizrate.com>
2017-12-10 21:00:42 -0800

    moving content from /about/who to /about

--- htdocs/about/who/index.php
+++ htdocs/about/who/index.php
@@ -1,54 +1,5 @@
 <?php
 
-include('../../../lib/base.php');
-include('../../../lib/about.php');
-
-print_about_header();
-
-?>
-
-<h1>WHO WE ARE</h1>
-<div class="column left">
-
-<p>Over the past thirty-five years, <em>Flury Bryant Design Group</em>, originally doing business as <em>The Bob Flury Design Group</em>, has been commissioned to design and detail over one thousand two hundred fifty residential projects, ranging in size from small remodels and additions to large estate properties with multiple buildings. We work with a large group of consultants and builders, giving us the capability of handling all aspects of a project, from conception and budget-building, through site development, architectural and structural detail development, interior architecture and detailing, and ultimately, completed construction.</p>
-<p>Our preferred approach to preparation of our construction documents is unusual, in that we prefer to prepare all of the structural drawings ourselves, creating a set of documents that integrates the structural detailing with the architectural in a seamless, user-friendly fashion.</p>
-<p>Our design work is always sensitive to the specific needs and wishes of our clients. We listen to their goals, making the accomplishment of these goals our top priority.</p>
-<p>Our staff has had extensive experience working with the Planning and Building Departments of literally every municipality in the area. We know what it takes to get projects approved. If we don't have the answers to your zoning and building questions, we know who to contact at City Hall to get the answers.</p>
-
-</div>
-<div class="column right">
-
-<p>The following is a partial list of the jurisdictions in which we have worked, followed by the number of projects in each:</p>
-
-<ul>
-  <li>Atherton, 236</li>
-  <li>Belmont, 12</li>
-  <li>Burlingame, 7</li>
-  <li>Cupertino, 10</li>
-  <li>Hillsborough, 18</li>
-  <li>Los Altos, 59</li>
-  <li>Los Altos Hills, 75</li>
-  <li>Los Gatos, 72</li>
-  <li>Menlo Park, 149</li>
-  <li>Monte Sereno, 22</li>
-  <li>Palo Alto, 100</li>
-  <li>Portola Valley, 25</li>
-  <li>Redwood City, 25</li>
-  <li>San Carlos, 18</li>
-  <li>San Francisco, 20</li>
-  <li>San Jose, 70</li>
-  <li>San Mateo, 24</li>
-  <li>Saratoga, 50</li>
-  <li>Sunnyvale, 11</li>
-  <li>Woodside, 79</li>
-</ul>
-
-<p>Whether your preference is timeless, traditional design, state-of-the-art contemporary architecture, or seamless blending of new construction to existing, our staff of dedicated professionals has the resources and the passion to create an environment designed around your tastes, personality, and dreams, with the specific purpose of enhancing your lifestyle.</p>
-
-</div>
-
-<?php
-
-print_about_footer();
+header('Location: /about');
 
 ?>
