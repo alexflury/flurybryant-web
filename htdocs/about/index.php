6cb05f1ef5b77be787687015a3a927f86c16bf49
Alex Flury <aflury@bizrate.com>
2017-12-11 11:54:36 -0800

    adding staff to about page

--- htdocs/about/index.php
+++ htdocs/about/index.php
@@ -52,6 +52,51 @@ print_header(array('page_classname' => 'about-page', 'banner_title' => 'WHO WE A
 <p>We strive to train our staff members in the artistic tradition of the design professions, as well as the latest state-of-the-art technology, with the ambition of providing our clients with sophisticated and innovative design solutions presented in artistic, easy to understand formats. We endeavor to form partnerships with our clients, our employees, consultants, and the communities we help to shape, resulting in beautiful homes that exude pride and add value.</p>
 <p>Our goal is to become the regionally recognized industry leader, experiencing moderate growth and annual profitability, while bringing excitement, fun, and integrity to the home-building and remodeling experience.</p>
 
+
+<span class="section-title">OUR STAFF</span>
+
+<div class="column left">
+
+<h2>Bob Flury</h2>
+<p>Bob Flury is one of those lucky people who never had to worry about what career path he would follow. As a child, he would draw elaborate pictures of houses, complete with floor plans and elevations. He knew that one day he would earn his living following his passion for home design.</p>
+<p>After studying architecture at West Valley College in Saratoga for two years, he sought employment in the local architectural community, working in a variety of specialties ranging from steel and concrete detailing, retail storefront design, commercial/industrial design, construction management, and, of course, home design.</p>
+<p>In 1979 he launched <em>The Bob Flury Design Group</em>. Since then, he has managed the design and construction of over seventeen hundred fifty residential projects, and is still going strong!</p>
+<p>Bob is married, with four grown children, and enjoys music and spending time at his beach retreat in Brookings, Oregon.</p>
+
+</div>
+<div class="column right">
+
+<h2>Bob Bryant</h2>
+<p>Bob Bryant has been involved in residential construction since 1976. He spent eight years as a carpenter, followed by five years as a construction project manager and, over the course of seventeen years provided design and detailing services on a variety of residential projects throughout the Bay Area. During that time, he created and managed the architectural department for a leading remodeling contractor in the South Bay.</p>
+<p>Bob joined <em>The Bob Flury Design Group</em> in 1993 as a senior draftsman. He became a partner in conjunction with the firm's incorporation in 2005, thereby forming <em>Flury Bryant Design Group, Inc.</em></p>
+<p>Bob is married and has three daughters. He enjoys golf, the sea, and he gets a great deal of satisfaction from being part of a team making contributions to the Bay Area's architectural landscape.</p>
+
+</div>
+<div class="column left">
+
+<h2>Mike Pursell</h2>
+
+<p>Mike's passion for architecture was sparked when, as a young boy, his grandmother gave him an art set with floor plans and perspective views of houses and other buildings. He pursued his interest in creating architectural drawings through his high school years where he developed CAD drafting skills and went on to compete and win in local and statewide drafting competitions.</p>
+<p>Mike has since gone on to apply his superior drafting skills to a variety of projects and disciplines ranging from residential architecture to electrical, plumbing, and mechanical engineering. He joined our team in 2004. His past experience working with software engineers and computer programmers has made him an invaluable asset to <em>Flury Bryant Design Group</em> as we work to continually upgrade our CAD-based drafting technology.</p>
+<p>Mike is married, with two children, and enjoys spending his free time on family bicycling outings and riding at the BMX track.</p>
+
+</div>
+<div class="column right">
+
+<h2>Dan Blankenbiller</h2>
+
+<p>Dean joined the <em>Flury Bryant Design Group</em> in 2013 following fifteen years as a design/build general contractor in the Sacramento foothills of California. He brings a tremendous amount of CAD design and drafting experience to our group, as well as many years of hands-on construction experience building new homes as well as remodel projects.</p>
+<p>In addition to his passion for fine home design and construction, Dean enjoys making music and designing and building custom guitars, and is a member of the Northern California Luthier Association.</p>
+<p>Dean is married, with three children and three grandchildren.</p>
+
+</div>
+<div class="column left">
+
+<h2>Albert Alfonzo</h2>
+
+<p>Prior to joining Flury Bryant Design Group in 2013, Albert spent 21 years gaining experience in the field of architecture in numerous bay area architectural and structural engineering firms, as well as international firms in Saudi Arabia. His skills include both CAD and hand-drafting techniques, as well as 3-D modeling.</p>
+<p>Albert received his Bachelor of Science in Architecture degree in the Philippines in 1992. He is married, with three children, and is an avid San Francisco Giants fan.</p>
+
 <?php
 
 print_footer();
