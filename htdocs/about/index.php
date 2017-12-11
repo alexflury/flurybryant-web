c96fe8d1fa2725c1dda539dfbf6c0510697b2186
Alex Flury <aflury@bizrate.com>
2017-12-11 11:47:15 -0800

    adding philosophy section to about page

--- htdocs/about/index.php
+++ htdocs/about/index.php
@@ -39,6 +39,12 @@ print_header(array('page_classname' => 'about-page', 'banner_title' => 'WHO WE A
 
 <p>Whether your preference is timeless, traditional design, state-of-the-art contemporary architecture, or seamless blending of new construction to existing, our staff of dedicated professionals has the resources and the passion to create an environment designed around your tastes, personality, and dreams, with the specific purpose of enhancing your lifestyle.</p>
 
+<h1>OUR OPERATING PHILOSOPHY</h1>
+
+<p><em>Flury Bryant Design Group</em> aspires to be the local industry leader, providing architectural services to the local custom residential market. Our goal is to provide our clients with the highest level of service-oriented project management, the finest personalized design in the industry, and undisputed value for every dollar spent.</p>
+<p>To achieve these goals, our design solutions tend to continuously evolve from initial conceptualization, through completed construction documents and often through the construction process. There is rarely a point in the process where we "complete" the design and move on to detailing.</p>
+<p>Through the years, this approach has resulted in a level of design and client satisfaction far above industry standards. Our clients, many of whom are extremely gifted designers and innovators in their own fields, tend to be very proactive throughout the design process. We encourage our clients to participate with us at this level in the hope that they will achieve the same level of affinity for their project that we strive for.</p>
+
 <?php
 
 print_footer();
