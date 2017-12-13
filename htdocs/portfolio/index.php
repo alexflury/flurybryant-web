c016f78a7f2104d2139e07b4355e680a6b1748d5
Alex Flury <aflury@bizrate.com>
2017-12-12 17:21:58 -0800

    getting the list of photos for portfolio page

--- htdocs/portfolio/index.php
+++ htdocs/portfolio/index.php
@@ -5,6 +5,7 @@ include('../../lib/portfolio.php');
 
 print_header(array('page_classname' => 'portfolio-page'));
 
+$slideshow_photos = get_photos(array('new-homes', 'remodels', 'interiors', 'details'));
 $slideshow_has_photo_picker = true;
 include('../../templates/slideshow.php');
 
