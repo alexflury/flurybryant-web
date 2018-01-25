15bad9985a7c3f88de267f2c63b611ecea79d708
Alex Flury <aflury@bizrate.com>
2018-01-24 16:41:49 -0800

    logging to show if photo sequence rect is defined

--- htdocs/js/photoSequence.js
+++ htdocs/js/photoSequence.js
@@ -275,8 +275,10 @@ FB.Modules.PhotoSequence.prototype = {
     var photoSequenceRect = this.html.getBoundingClientRect();
     console.log('photoSequenceRect=' + JSON.stringify(photoSequenceRect));
     if (photoSequenceRect === undefined) {
+      console.log('photoSequenceRect is undefined');
       return;
     }
+    console.log('photoSequenceRect is defined');
     var frameRect = frame.html.getBoundingClientRect();
     var height = photoSequenceRect.height * Math.pow(2, frame.zoomLevel);
     var width = photoSequenceRect.width * Math.pow(2, frame.zoomLevel);
